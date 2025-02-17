2cf24a7822487b070816430a46f0398fcc608ca6
athledz <athleticsdznewz@gmail.com>
2025-02-17 08:00:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-// Gestion du formulaire de contact
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Empêche le rechargement de la page
-
-    const nom = document.querySelector('input[type="text"]').value;
-    const email = document.querySelector('input[type="email"]').value;
-    const message = document.querySelector('textarea').value;
-
-    if (nom && email && message) {
-        alert(`Merci ${nom} ! Votre message a été envoyé.`);
-        this.reset(); // Réinitialise le formulaire
-    } else {
-        alert('Veuillez remplir tous les champs.');
-    }
-});
-// Sélectionner le bouton hamburger et le conteneur du navbar
+// Sélectionner le bouton hamburger et la liste de navigation
 const hamburgerMenu = document.getElementById('hamburger-menu');
-const navContainer = document.getElementById('nav-container');
+const navLinks = document.getElementById('nav-links');
 
 // Ajouter un gestionnaire d'événements au clic
 hamburgerMenu.addEventListener('click', () => {
-    // Alterner la classe "active" sur le nav-container
-    navContainer.classList.toggle('active');
+    // Alterner la classe "active" sur la liste de navigation
+    navLinks.classList.toggle('active');
 });
